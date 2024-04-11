cac0142bab3523b27f6b6a8e80b74dc87532daaf
CHAPshushan <166685568+CHAPshushan@users.noreply.github.com>
2024-04-11 15:34:34 +1000

    Enrolment.ps1

--- Enrolment.ps1
+++ Enrolment.ps1
@@ -1,19 +1,7 @@
 # Set execution policy to bypass
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
+Set-ExecutionPolicy Bypass -Force
 
 # Install the Get-WindowsAutoPilotinfo script from PowerShell Gallery
-Install-Script -Name Get-WindowsAutoPilotinfo -Force -AcceptLicense
+Install-Script -Name Get-WindowsAutoPilotinfo
 
-# Import the module after installation
-Import-Module -Name Get-WindowsAutoPilotinfo -Force
-
-# Define a function to simulate "Yes" response
-function Select-Yes {
-    return "Yes"
-}
-
-# Override Read-Host function to automatically select "Yes"
-$function:Read-Host = ${function:Select-Yes}
-
-# Run the Get-WindowsAutoPilotinfo script with online option
 Get-WindowsAutoPilotinfo.ps1 -Online
